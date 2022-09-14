97512354c1a9af35252cfe46f74f0793f0633ae6
TheAppLabb <info@theapplabb.com>
2022-09-14 21:34:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+  
+export default class PhoneInputGfg extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { phone: "" };
+  }
+  render(){
+    return (
+      <div>
+        <h1>ReactJs Phone Input - Sumit</h1>
+        <PhoneInput
+          country={'us'}
+          value={this.state.phone}
+          onChange={phone => this.setState({ phone })}
+        />
+      </div>
+    );
+  }
+};
